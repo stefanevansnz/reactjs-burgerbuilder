bce54d26a43d3eb6839a5e3112ef3a2335146311
Stefan Evans <stefan.evans@consegna.cloud>
2021-04-10 11:25:09 +1200

    added price to burger controller

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -65,7 +65,9 @@ class BurgerBuilder extends Component {
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler} 
                     ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}/>                
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                    />                
             </Aux>
         );
     }
